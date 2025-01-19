d1dd4cae9842e2c76a8eb74022558246c7e29ff4
CristianLB <cristian.lb.08@gmail.com>
2025-01-19 04:10:15 -0300

    Projeto finalizado

--- Trilha-DIO-Java-basico/src/ContaTerminal.java
+++ Trilha-DIO-Java-basico/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Insira o numero da Conta: ");
+    int numeroConta = scanner.nextInt();
+
+    System.out.println("Insira o numero da agencia: ");
+    String agencia = scanner.nextLine();
+
+    System.out.println("Insira o nome do cliente:");
+    String nomeClinete = scanner.nextLine();
+
+    System.out.println("Insira o saldo inicial da conta: ");
+    double saldoConta = scanner.nextDouble();
+
+    System.out.println("Olá "+ nomeClinete + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta" + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque");
+
+scanner.close();
+
     }
 }
